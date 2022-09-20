a513550546042d55cf3d114d51e7238874071d6c
king stark <63186186+King-stark@users.noreply.github.com>
2022-09-20 13:27:13 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,5 @@
 
 sed -i "s#https://s.ops.ci#https://${MYURLS_DOMAIN}#g" /app/public/index.html
 
-/app/myurls -domain ${MYURLS_DOMAIN} -conn $RDSHOST:$RDSPORT -passwd $RDSPASSWORD -ttl ${MYURLS_TTL}
-
+/app/myurls -domain ${MYURLS_DOMAIN} -conn $RDSHOST:$RDSPORT -passwd $RDSPASSWORD -ttl ${MYURLS_TTL} &
 nginx -g 'daemon off;'
